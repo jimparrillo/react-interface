fc553bfe10bbc06bdd6692617fdb5c6b9e740a78
Jim Parrillo <james.parrillo@gmail.com>
2022-07-09 13:44:41 -0400

    installed and configured tailwind per video 1_05 but tailwind is not working properly.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { MdDeleteForever } from "react-icons/md"
 
 function App() {
   return (
-    <div className="App">
-      <h1><MdDeleteForever />Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl"><MdDeleteForever className="inline-block text-red-400 align-top" />Your Appointments</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], 
+  darkMode: false,
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [ require('@tailwindcss/forms')]
 }
